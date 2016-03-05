de30145fde0a7e3b2ab98cba159425cff012bb5a
piotrekzyla <kontakt@piotrzyla.pl>
2016-03-05 14:54:58 +0530

    translate.rb update

--- translate.rb
+++ translate.rb
@@ -1,7 +1,6 @@
 require 'nokogiri'
 require 'csv'
 
-targets= ['czech', 'slovak', 'hungarian', 'polish']
 
 trans = CSV.read("translate.csv")
 headers = trans[0]
@@ -12,45 +11,29 @@ trans[1..-1].each do |phrase|
   headers[1..-1].each_with_index do |lang, i|
     p[lang] = phrase[i+1]
   end
-  next if phrase[0].nil?
-  translations[phrase[0].strip] = p
+  translations[phrase[0]] = p
 end
 
-targets.each do |target|
+headers[1..-1].each do |target|
+  dom = Nokogiri::HTML.parse open("index.html").read()
 
-dom = Nokogiri::HTML.parse open("index.html").read()
+  open("strings.en.txt", "w") do |stringsOut|
+    dom.xpath('//text()').map do |txt|
+      s = txt.content
+      s = s.strip
+      next if s == ''
+      stringsOut.write s + "\n"
 
-open("strings.en.txt", "w") do |stringsOut|
-  dom.xpath('//text()').map do |txt|
-    s = txt.content
-    s = s.strip
-    next if s == ''
-    stringsOut.write s + "\n"
-
-    t = translations[s]
-    if t.nil?
-      puts "ERROR! no #{target} for #{s}"
-    else
-      puts "#{t}==#{t[target]}"
-      txt.content = t[target]
+      t = translations[s]
+      if t.nil?
+        puts "ERROR! no text for #{s}"
+      else
+        puts "#{t}==#{t[target]}"
+        txt.content = t[target]
+      end
     end
   end
-  #require 'pry'
-  # binding.pry
-  button = dom.xpath("//*[@type='submit']").first
-  puts "A! #{button} #{button['value']}"
-  t = translations[button["value"]]
-  if t.nil?
-    puts "ERROR! no #{target} for #{button['value']}"
-  else
-    puts "#{t}==#{t[target]}"
-    button["value"] = t[target]
-  end
 
-end
 
-#puts dom.to_html
-
-dom.write_html_to(File.new("index.#{target}.html", 'w'), :encoding => 'UTF-8')
-
+  dom.write_html_to(File.new("index.#{target}.html", 'w'), :encoding => 'UTF-8')
 end
